ae3d889c91fbe3e1969d69b94747c748d8a669fa
J3ffJessie <jejess6125@gmail.com>
2021-01-11 20:36:13 -0600

    Pull changes from main with other branches file input

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,23 +15,19 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-<<<<<<< HEAD
+
     <Link to="/about/">About Page</Link> <br />
     <Link to="/thomas/">Thomas's Page</Link> <br />
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/greg/">Go to Greg's page</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-<<<<<<< HEAD
-=======
     <Link to="/page-2">Go to page 2</Link> <br />
     <Link to="/jeff">Jeff Page</Link>
->>>>>>> JeffJ
-=======
+
     <img src="https://source.unsplash.com/random" alt="" />
         <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
             <NewImage />
         </div>
->>>>>>> 8686352aac01d5679ba78dee43f8684e2b262a14
   </Layout>
 )
 
