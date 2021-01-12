f34e1baa545e65543b2f6fa1d42d804c51110ff6
ChrisKnowsDev <chrisharley81@gmail.com>
2021-01-11 20:57:17 -0500

    completed img component task

--- src/pages/chris.js
+++ src/pages/chris.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import Img from "../components/img"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -9,6 +10,9 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <h1>Chris's Page</h1>
     <p>Welcome to page 2</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Img />
+    </div>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
