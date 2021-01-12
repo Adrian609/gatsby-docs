106773efcb61b8fc8d8bba11bd95ad8482de52d0
J3ffJessie <jejess6125@gmail.com>
2021-01-11 19:24:23 -0600

    Added My Page to Link section on index.js pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,8 +14,8 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Link to="/about">About</Link> <br />
+    <Link to="/Jeff">Jeff Page</Link>
   </Layout>
 )
 
